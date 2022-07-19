e8c32ceafdc898491e96c87b0d499a7dce5975cc
B0RnInUSSR <105313334+B0RnInUSSR@users.noreply.github.com>
2022-07-19 07:04:15 +0300

    docs: update code in file 'index.js'

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let character = '';
+    let sliceCharacter = '';
+    let result = '';
+
+    for (let i = 0; i < expr.length; i = i + 10) {
+        let resultInDot = '';
+        character = expr.slice(i, i+10);
+       
+        branch: { 
+            for (let j = 0; j < character.length; j++) {
+            
+            if (character[j] === '1') {
+                sliceCharacter = character.slice(j);
+                break branch;
+                }
+            }        
+        }
+        if (character === '**********') {
+            result = result + ' ';
+            sliceCharacter='00';
+        }
+
+        for (let k = 0; k < sliceCharacter.length; k = k + 2) {
+            let dotResult = '';
+
+            if (sliceCharacter.slice(k, k+2) === '10') {
+                dotResult = '.';                    
+            }
+            else if (sliceCharacter.slice(k, k+2) === '11') {
+                dotResult = '-';
+            }
+            
+            if (dotResult !== '') {
+            resultInDot = resultInDot + dotResult;
+            }
+       
+        }
+        if (resultInDot !== '') { 
+        result = result + MORSE_TABLE[resultInDot];
+        }
+    }
+    return result;
 }
 
 module.exports = {
